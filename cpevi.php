2e0f0dc72f995c40590634e4f1b2524bb4130eeb
gbarbarino <dr.gwydion@live.it>
2017-04-20 14:23:18 +0200

    Insert text domain

--- cpevi.php
+++ cpevi.php
@@ -1,2 +1,26 @@
 <?php
-?>
+/*
+Plugin Name: CpEvi PDF
+Plugin URI: https://evidenza.itDescription: Require from users to confirm email upon registration in AppThemes themes.
+
+Version: 1.4.20
+Author: EvidenzaIt
+Author URI: https://evidenza.it/chi-siamo*
+Text Domain: cpevi-pdf
+Domain Path: /languages
+*/
+
+define( 'CE_TD', 'cpevi-pdf' );
+
+// Avoid calling file directly.
+if ( ! function_exists( 'add_action' ) ) {
+	die( 'Whoops! You shouldn\'t be doing that.' );
+}
+
+
+// Load Text-Domain.
+ function cpevi_textdomain() {
+	load_plugin_textdomain( CE_TD, false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+}
+add_action( 'plugins_loaded', 'cpevi_textdomain' );
+
